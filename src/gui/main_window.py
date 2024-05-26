825c8535e647adf4c33b12f440c9cee2f7c3c8d0
Abraamyan Alexander <samvelcc25@outlook.com>
2024-05-26 15:32:38 +0300

    buttons connected

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -2,7 +2,6 @@ from servers.base_server import BaseServer
 from .ui_main_window import Ui_MainWindow
 
 from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtCore import QTimer
 
 LEFT_WING_PORT = 32001
 RIGHT_WING_PORT = LEFT_WING_PORT + 1
@@ -24,3 +23,23 @@ class MainWindow(QMainWindow):
         self.right_wing_server.start_server_thread()
         self.left_stabilizer_server.start_server_thread()
         self.right_stabilizer_server.start_server_thread()
+
+        self._ui.lw_1m_btn.clicked.connect(lambda: self.left_wing_server.set_distance(1))
+        self._ui.lw_2m_btn.clicked.connect(lambda: self.left_wing_server.set_distance(2))
+        self._ui.lw_3m_btn.clicked.connect(lambda: self.left_wing_server.set_distance(3))
+        self._ui.lw_safe_btn.clicked.connect(lambda: self.left_wing_server.set_distance(100))
+
+        self._ui.rw_1m_btn.clicked.connect(lambda: self.right_wing_server.set_distance(1))
+        self._ui.rw_2m_btn.clicked.connect(lambda: self.right_wing_server.set_distance(2))
+        self._ui.rw_3m_btn.clicked.connect(lambda: self.right_wing_server.set_distance(3))
+        self._ui.rw_safe_btn.clicked.connect(lambda: self.right_wing_server.set_distance(100))
+
+        self._ui.ls_1m_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(1))
+        self._ui.ls_2m_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(2))
+        self._ui.ls_3m_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(3))
+        self._ui.ls_safe_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(100))
+
+        self._ui.rs_1m_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(1))
+        self._ui.rs_2m_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(2))
+        self._ui.rs_3m_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(3))
+        self._ui.rs_safe_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(100))
