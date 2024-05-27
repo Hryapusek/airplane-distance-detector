73a486ca0c7fc9e41fa82a82c27e53403c93e4c4
Abraamyan Alexander <samvelcc25@outlook.com>
2024-05-27 23:37:34 +0300

    colors added

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,12 +1,17 @@
 from servers.base_server import BaseServer
 from .ui_main_window import Ui_MainWindow
 
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QPushButton
 
 LEFT_WING_PORT = 32105
 RIGHT_WING_PORT = LEFT_WING_PORT + 1
 LEFT_STABILIZER_PORT = RIGHT_WING_PORT + 1
 RIGHT_STABILIZER_PORT = LEFT_STABILIZER_PORT + 1
+
+M1_COLOR = "red"
+M2_COLOR = "orange"
+M3_COLOR = "yellow"
+SAFE_COLOR = "green"
 
 
 class MainWindow(QMainWindow):
@@ -24,22 +29,153 @@ class MainWindow(QMainWindow):
         self.left_stabilizer_server.start_server_thread()
         self.right_stabilizer_server.start_server_thread()
 
-        self._ui.lw_1m_btn.clicked.connect(lambda: self.left_wing_server.set_distance(1))
-        self._ui.lw_2m_btn.clicked.connect(lambda: self.left_wing_server.set_distance(2))
-        self._ui.lw_3m_btn.clicked.connect(lambda: self.left_wing_server.set_distance(3))
-        self._ui.lw_safe_btn.clicked.connect(lambda: self.left_wing_server.set_distance(100))
+        self._ui.lw_1m_btn.clicked.connect(self.lw_1m_btn_clicked)
+        self._ui.lw_2m_btn.clicked.connect(self.lw_2m_btn_clicked)
+        self._ui.lw_3m_btn.clicked.connect(self.lw_3m_btn_clicked)
+        self._ui.lw_safe_btn.clicked.connect(self.lw_safe_btn_clicked)
 
-        self._ui.rw_1m_btn.clicked.connect(lambda: self.right_wing_server.set_distance(1))
-        self._ui.rw_2m_btn.clicked.connect(lambda: self.right_wing_server.set_distance(2))
-        self._ui.rw_3m_btn.clicked.connect(lambda: self.right_wing_server.set_distance(3))
-        self._ui.rw_safe_btn.clicked.connect(lambda: self.right_wing_server.set_distance(100))
+        self._ui.rw_1m_btn.clicked.connect(self.rw_1m_btn_clicked)
+        self._ui.rw_2m_btn.clicked.connect(self.rw_2m_btn_clicked)
+        self._ui.rw_3m_btn.clicked.connect(self.rw_3m_btn_clicked)
+        self._ui.rw_safe_btn.clicked.connect(self.rw_safe_btn_clicked)
 
-        self._ui.ls_1m_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(1))
-        self._ui.ls_2m_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(2))
-        self._ui.ls_3m_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(3))
-        self._ui.ls_safe_btn.clicked.connect(lambda: self.left_stabilizer_server.set_distance(100))
+        self._ui.ls_1m_btn.clicked.connect(self.ls_1m_btn_clicked)
+        self._ui.ls_2m_btn.clicked.connect(self.ls_2m_btn_clicked)
+        self._ui.ls_3m_btn.clicked.connect(self.ls_3m_btn_clicked)
+        self._ui.ls_safe_btn.clicked.connect(self.ls_safe_btn_clicked)
 
-        self._ui.rs_1m_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(1))
-        self._ui.rs_2m_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(2))
-        self._ui.rs_3m_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(3))
-        self._ui.rs_safe_btn.clicked.connect(lambda: self.right_stabilizer_server.set_distance(100))
+        self._ui.rs_1m_btn.clicked.connect(self.rs_1m_btn_clicked)
+        self._ui.rs_2m_btn.clicked.connect(self.rs_2m_btn_clicked)
+        self._ui.rs_3m_btn.clicked.connect(self.rs_3m_btn_clicked)
+        self._ui.rs_safe_btn.clicked.connect(self.rs_safe_btn_clicked)
+
+        self.lw_safe_btn_clicked()
+        self.rw_safe_btn_clicked()
+        self.ls_safe_btn_clicked()
+        self.rs_safe_btn_clicked()
+
+    def set_button_color(self, btn: QPushButton, color: str):
+        btn.setStyleSheet(f"background-color: {color}")
+
+    def rw_clear_colors(self):
+        self._ui.rw_1m_btn.setStyleSheet(None)
+        self._ui.rw_2m_btn.setStyleSheet(None)
+        self._ui.rw_3m_btn.setStyleSheet(None)
+        self._ui.rw_safe_btn.setStyleSheet(None)
+
+    def lw_clear_colors(self):
+        self._ui.lw_1m_btn.setStyleSheet(None)
+        self._ui.lw_2m_btn.setStyleSheet(None)
+        self._ui.lw_3m_btn.setStyleSheet(None)
+        self._ui.lw_safe_btn.setStyleSheet(None)
+
+    def rs_clear_colors(self):
+        self._ui.rs_1m_btn.setStyleSheet(None)
+        self._ui.rs_2m_btn.setStyleSheet(None)
+        self._ui.rs_3m_btn.setStyleSheet(None)
+        self._ui.rs_safe_btn.setStyleSheet(None)
+
+    def ls_clear_colors(self):
+        self._ui.ls_1m_btn.setStyleSheet(None)
+        self._ui.ls_2m_btn.setStyleSheet(None)
+        self._ui.ls_3m_btn.setStyleSheet(None)
+        self._ui.ls_safe_btn.setStyleSheet(None)
+
+    def lw_1m_btn_clicked(self):
+        self.left_wing_server.set_distance(1)
+        self.lw_clear_colors()
+        self.set_button_color(self._ui.lw_1m_btn, M1_COLOR)
+        self.update()
+
+    def lw_2m_btn_clicked(self):
+        self.left_wing_server.set_distance(2)
+        self.lw_clear_colors()
+        self.set_button_color(self._ui.lw_2m_btn, M2_COLOR)
+        self.update()
+
+    def lw_3m_btn_clicked(self):
+        self.left_wing_server.set_distance(3)
+        self.lw_clear_colors()
+        self.set_button_color(self._ui.lw_3m_btn, M3_COLOR)
+        self.update()
+
+    def lw_safe_btn_clicked(self):
+        self.left_wing_server.set_distance(100)
+        self.lw_clear_colors()
+        self.set_button_color(self._ui.lw_safe_btn, SAFE_COLOR)
+        self.update()
+
+
+    def rw_1m_btn_clicked(self):
+        self.right_wing_server.set_distance(1)
+        self.rw_clear_colors()
+        self.set_button_color(self._ui.rw_1m_btn, M1_COLOR)
+        self.update()
+
+    def rw_2m_btn_clicked(self):
+        self.right_wing_server.set_distance(2)
+        self.rw_clear_colors()
+        self.set_button_color(self._ui.rw_2m_btn, M2_COLOR)
+        self.update()
+
+    def rw_3m_btn_clicked(self):
+        self.right_wing_server.set_distance(3)
+        self.rw_clear_colors()
+        self.set_button_color(self._ui.rw_3m_btn, M3_COLOR)
+        self.update()
+
+    def rw_safe_btn_clicked(self):
+        self.right_wing_server.set_distance(100)
+        self.rw_clear_colors()
+        self.set_button_color(self._ui.rw_safe_btn, SAFE_COLOR)
+        self.update()
+
+
+    def ls_1m_btn_clicked(self):
+        self.left_stabilizer_server.set_distance(1)
+        self.ls_clear_colors()
+        self.set_button_color(self._ui.ls_1m_btn, M1_COLOR)
+        self.update()
+
+    def ls_2m_btn_clicked(self):
+        self.left_stabilizer_server.set_distance(2)
+        self.ls_clear_colors()
+        self.set_button_color(self._ui.ls_2m_btn, M2_COLOR)
+        self.update()
+
+    def ls_3m_btn_clicked(self):
+        self.left_stabilizer_server.set_distance(3)
+        self.ls_clear_colors()
+        self.set_button_color(self._ui.ls_3m_btn, M3_COLOR)
+        self.update()
+
+    def ls_safe_btn_clicked(self):
+        self.left_stabilizer_server.set_distance(100)
+        self.ls_clear_colors()
+        self.set_button_color(self._ui.ls_safe_btn, SAFE_COLOR)
+        self.update()
+
+
+    def rs_1m_btn_clicked(self):
+        self.right_stabilizer_server.set_distance(1)
+        self.rs_clear_colors()
+        self.set_button_color(self._ui.rs_1m_btn, M1_COLOR)
+        self.update()
+
+    def rs_2m_btn_clicked(self):
+        self.right_stabilizer_server.set_distance(2)
+        self.rs_clear_colors()
+        self.set_button_color(self._ui.rs_2m_btn, M2_COLOR)
+        self.update()
+
+    def rs_3m_btn_clicked(self):
+        self.right_stabilizer_server.set_distance(3)
+        self.rs_clear_colors()
+        self.set_button_color(self._ui.rs_3m_btn, M3_COLOR)
+        self.update()
+
+    def rs_safe_btn_clicked(self):
+        self.right_stabilizer_server.set_distance(100)
+        self.rs_clear_colors()
+        self.set_button_color(self._ui.rs_safe_btn, SAFE_COLOR)
+        self.update()
